5fa7160cfc8d57a10d0cd48728a560d3924c9aa6
fabiohxcx <hideki.fabio@gmail.com>
2018-01-30 14:07:56 -0200

    created recipe details screen

--- app/src/main/java/fabiohideki/com/tastedacity/RecipeDetailsActivity.java
+++ app/src/main/java/fabiohideki/com/tastedacity/RecipeDetailsActivity.java
@@ -2,30 +2,89 @@ package fabiohideki.com.tastedacity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.parceler.Parcels;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import fabiohideki.com.tastedacity.adapter.StepsAdapter;
+import fabiohideki.com.tastedacity.model.Ingredient;
 import fabiohideki.com.tastedacity.model.Recipe;
+import fabiohideki.com.tastedacity.model.Step;
 
 public class RecipeDetailsActivity extends AppCompatActivity {
 
     @BindView(R.id.tv_recipe_details)
     TextView textView;
 
+    @BindView(R.id.bt_ingredients)
+    Button btIngredients;
+
+    @BindView(R.id.recyclerview_steps)
+    RecyclerView mRecyclerView;
+
+
+    private Recipe recipe;
+    private List<Step> steps;
+    private List<Ingredient> ingredients;
+    private StepsAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
         ButterKnife.bind(this);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        Recipe recipe = (Recipe) Parcels.unwrap(getIntent().getParcelableExtra("recipe"));
+        recipe = (Recipe) Parcels.unwrap(getIntent().getParcelableExtra("recipe"));
 
-        if (recipe != null)
+        if (recipe != null) {
+
+            setTitle(recipe.getName());
+
+            steps = recipe.getSteps();
+            ingredients = recipe.getIngredients();
+
             textView.setText(recipe.getName());
+        }
 
+        setupRecycler();
 
     }
+
+    private void setupRecycler() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mRecyclerView.getContext(),
+                layoutManager.getOrientation());
+        dividerItemDecoration.setDrawable(getDrawable(R.drawable.line));
+
+        mRecyclerView.addItemDecoration(dividerItemDecoration);
+
+        mAdapter = new StepsAdapter(steps, this);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+
+    @OnClick(R.id.bt_ingredients)
+    public void callIngredientsScreen() {
+        Toast.makeText(this, ingredients.get(0).getIngredient(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
+
 }
---
+++ app/src/main/java/fabiohideki/com/tastedacity/adapter/StepsAdapter.java
@@ -0,0 +1,79 @@
+package fabiohideki.com.tastedacity.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import fabiohideki.com.tastedacity.R;
+import fabiohideki.com.tastedacity.model.Step;
+
+/**
+ * Created by fabio.lagoa on 30/01/2018.
+ */
+
+public class StepsAdapter extends RecyclerView.Adapter<StepsAdapter.StepsHolder> {
+
+    private List<Step> steps;
+    private Context context;
+
+    public StepsAdapter(List<Step> steps, Context context) {
+        this.steps = steps;
+        this.context = context;
+    }
+
+    @Override
+    public StepsAdapter.StepsHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new StepsHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_step, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(StepsAdapter.StepsHolder holder, int position) {
+
+        if (steps != null && !steps.isEmpty()) {
+            Step step = steps.get(position);
+            holder.stepShortDesc.setText(step.getShortDescription());
+            holder.stepId.setText("#" + (step.getId() + 1));
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return steps != null ? steps.size() : 0;
+    }
+
+    public class StepsHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        @BindView(R.id.layout_item_step)
+        LinearLayout layoutStep;
+
+        @BindView(R.id.tv_step_id)
+        TextView stepId;
+
+        @BindView(R.id.tv_step_short_desc)
+        TextView stepShortDesc;
+
+
+        public StepsHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            layoutStep.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(context, steps.get(getAdapterPosition()).getDescription().toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
